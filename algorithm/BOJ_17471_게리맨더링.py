f47725944457eb7e66329614887a1b46a996913b
yjp8842 <yjp20183029@gmail.com>
2022-09-29 00:04:18 +0900

    algorithm

---
+++ algorithm/BOJ_17471_게리맨더링.py
@@ -0,0 +1,9 @@
+N = int(input())
+person = list(map(int, input().split()))
+city_list = []
+for _ in range(N):
+    city_list.append(list(map(int, input().split())))
+
+city = []
+for i in range(len(city_list)):
+    pass
