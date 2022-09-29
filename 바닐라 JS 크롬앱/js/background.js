a1c15e5ada4e11d60dfd4dfdd654f17b955719be
yjp8842 <yjp20183029@gmail.com>
2022-09-30 00:22:52 +0900

    .

--- 바닐라 JS 크롬앱/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["img1.jpg", "img2.jpg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
