dafcd95e45c73ccb9bcc060020376e405f989b1e
yjp8842 <109319379+yjp8842@users.noreply.github.com>
2023-06-25 22:45:34 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 380 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,33 @@
+while 1:
+  str_s = list(map(str, input()))
+  
+  if str_s[0] == '.':
+    break
+
+  tests = []
+  for i in range(len(str_s)):
+    if str_s[i] == ')':
+      if tests and tests[len(tests) - 1] == '(':
+        tests.pop()
+      else:
+        tests.append(str_s[i])
+        break
+    elif str_s[i] == '(':
+      tests.append(str_s[i])
+    
+    elif str_s[i] == ']':
+      if tests and tests[len(tests) - 1] == '[':
+        tests.pop()
+      else:
+        tests.append(str_s[i])
+        break
+    elif str_s[i] == '[':
+      tests.append(str_s[i])
+    
+    else:
+      continue
+
+  if tests != []:
+    print('no')
+  else:
+    print('yes')
