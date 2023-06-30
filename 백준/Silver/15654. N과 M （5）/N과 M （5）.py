dc04be30ab0f8a4beceaf78c9c74cc1987735496
yjp8842 <109319379+yjp8842@users.noreply.github.com>
2023-07-01 01:04:28 +0900

    [Silver III] Title: N과 M (5), Time: 236 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,22 @@
+def solve(idx, N, M):
+  if idx == M:
+    for i in stack:
+      print(i, end=' ')
+    print()
+    return
+
+  for i in range(N):
+    if dat[i] == 1:
+      continue
+    stack.append(lists[i]) # 1번
+    dat[i] = 1
+    solve(idx + 1, N, M)  # 2번
+    stack.pop()     # 3번
+    dat[i] = 0
+
+N, M = map(int, input().split())
+lists = list(map(int, input().split()))
+lists.sort()
+stack = []
+dat = [0] * (N + 1)
+solve(0, N, M)
